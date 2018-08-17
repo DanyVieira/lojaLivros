d9fad50bf10ac6b0512712875b5a70fcde7f2d0e
Daniele <danielevf@gmail.com>
2018-08-17 10:43:35 -0300

    Começo

---
+++ src/main/java/com/alura/lojaLivros/lojaLivros/LojaLivrosApplication.java
@@ -0,0 +1,12 @@
+package com.alura.lojaLivros.lojaLivros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaLivrosApplication.class, args);
+	}
+}
